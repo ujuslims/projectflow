aab262063359bf1cb93eb68d53dc455a88dcf1cc
Uju Uduma Ikpa <ujuslims@gmail.com>
2025-05-27 10:33:24 +0000

    Update app

--- src/components/project/create-project-dialog.tsx
+++ src/components/project/create-project-dialog.tsx
@@ -48,7 +48,7 @@ export function CreateProjectDialog() {
   return (
     <Dialog open={isOpen} onOpenChange={setIsOpen}>
       <DialogTrigger asChild>
-        <Button>
+        <Button variant="destructive">
           <PlusCircle className="mr-2 h-4 w-4" /> New Project
         </Button>
       </DialogTrigger>
